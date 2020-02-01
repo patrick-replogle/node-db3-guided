9c111a08976b83714913e18c7c35df43c483117d
patrick-replogle <patrickr1138@gmail.com>
2020-02-01 14:06:57 -0800

    cleaned up something.

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
 
-const UserRouter = require('./users/user-router.js');
+const UserRouter = require("./users/user-router.js");
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 
-server.use('/api/users', UserRouter);
+server.use("/api/users", UserRouter);
 
 server.use((err, req, res, next) => {
-    console.log(err);
-    return res.status(500).json({
-      message: "Something went wrong. Try again later"
-    });
+  console.log(err);
+  return res.status(500).json({
+    message: "Something went wrong. Try again later"
+  });
+});
 
 module.exports = server;
